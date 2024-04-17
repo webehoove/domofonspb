c0ce72471a35c462c496a694b5b0f44259aca1c9
Stanislav <104698147+webehoove@users.noreply.github.com>
2024-04-17 10:46:31 +0200

    Add files via upload

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -83,7 +83,7 @@
 						'<span class="demo-controls">' +
 							(options.palette ?
 								'<span class="property active" data-name="scheme">' +
-									'<a href="#" class="title">Scheme</a>' +
+									'<a href="#" class="title"><img src="../../images/wb_scheme.svg"></a>' +
 									'<span class="classes">' +
 										'<span data-class="-" class="active">default</span>' +
 										'<span data-class="invert">invert</span>' +
